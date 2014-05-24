54e0e09733f9afef81c8052393b859fe6c65b7e9
Błażej Kotowski <blaziko@gmail.com>
2014-05-24 17:13:11 +0200

    beginning of pseudocode implementation

--- src/common.hpp
+++ src/common.hpp
@@ -6,4 +6,24 @@
 
 using namespace std;
 
+#define SIDES_NUM 4
+#define RES_NUM   2
+
+enum state {
+  IDLE = 0,
+  HAS_TOKEN
+};
+
+enum side {
+  SELF = 0,
+  LEFT,
+  RIGHT,
+  PARENT
+};
+
+enum resource {
+  WINDMILL = 0,
+  HORSE
+};
+
 #endif
