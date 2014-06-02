c39156aaa93bb6ab0cc31747092eafee221946f1
Michał Zajączkowski <michal.zajaczkowski@netguru.pl>
2014-06-02 07:19:26 +0200

    simple main

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,58 @@
 #include "common.hpp"
 #include "mpi_lock.hpp"
+#include "mpi_controller.hpp"
 
+#define TREE_RANK 2
+
+bool should_work = true;
+
+void stop_work(int sig) {
+  should_work = false;
+}
+
+unsigned random_unsigned(unsigned from, unsigned to) {
+  default_random_engine generator;
+  uniform_int_distribution<unsigned> distribution(from, to);
+  return distribution(generator);
+}
+
+void random_sleep(unsigned min_ms, unsigned max_ms) {
+  unsigned sleep_time = random_unsigned(min_ms, max_ms);
+  this_thread::sleep_for(chrono::milliseconds(sleep_time));
+}
+
+void application_process(MonitorLock *lock) {
+  while (should_work) {
+    lock->reserve(HORSE);
+    lock->reserve(WINDMILL);
+
+    random_sleep(400, 1000);
+
+    lock->release(HORSE);
+    lock->release(WINDMILL);
+
+    random_sleep(200, 1500);
+  }
+}
+
+void monitor_process(MPIController &controller) {
+  while (should_work) {
+    controller.handle_message();
+  }
+}
 
 int main(int argc, char **argv) {
-  int size,                          // MPI Processes number
-      rank;                          // MPI Process ID
+  thread application_thread;
+  MPIController controller(&argc, argv, TREE_RANK);
 
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  controller.add_resource(HORSE);
+  controller.add_resource(WINDMILL);
 
-  // while (working) {
+  // signal(SIGINT, stop_work);
 
-    request_resource(HORSE);
-    request_resource(WINDMILL);
+  application_thread = thread(application_process, controller.get_lock());
+  monitor_process(controller);
+  application_thread.join();
 
-    // fight
-
-    release_resource(WINDMILL);
-    release_resource(HORSE);
-
-  // }
-
-  // sync
-  // clean
-
-  MPI_Finalize();
   return 0;
 }
